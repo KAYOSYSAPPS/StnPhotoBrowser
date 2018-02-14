70574c08e519d17c98c7371da60ad8cdc24da393
bhupinderKayo <bhupinder.singh@kayosys.com>
2018-02-14 15:00:39 +0530

    Initial Commit

---
+++ Example/Pods/Target Support Files/Pods-StnPhotoBrowser_Tests/Pods-StnPhotoBrowser_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StnPhotoBrowser_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_StnPhotoBrowser_Tests
+@end
---
+++ Example/Pods/Target Support Files/StnPhotoBrowser/StnPhotoBrowser-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_StnPhotoBrowser : NSObject
+@end
+@implementation PodsDummy_StnPhotoBrowser
+@end
