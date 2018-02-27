f43a0cd68ff03dae0bafe0d08241ca9095e849f4
bhupinderKayo <bhupinder.singh@kayosys.com>
2018-02-27 15:39:56 +0530

    e

---
+++ StnPhotoBrowser/Classes/extensions/UIView+Radius.swift
@@ -0,0 +1,19 @@
+//
+//  UIView+Radius.swift
+//  StnPhotoBrowser
+//
+//
+
+import UIKit
+
+extension UIView {
+    func addCornerRadiusAnimation(_ from: CGFloat, to: CGFloat, duration: CFTimeInterval) {
+        let animation = CABasicAnimation(keyPath: "cornerRadius")
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        animation.fromValue = from
+        animation.toValue = to
+        animation.duration = duration
+        self.layer.add(animation, forKey: "cornerRadius")
+        self.layer.cornerRadius = to
+    }
+}
